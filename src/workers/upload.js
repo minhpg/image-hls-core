897e30dc5fb9aa5e704bf62b5916ed4fdaf20cd0
minhpg <67231681+minhpg@users.noreply.github.com>
2022-05-30 17:48:02 +0700

    fix

--- src/workers/upload.js
+++ src/workers/upload.js
@@ -6,7 +6,7 @@ const fileSchema = require('../models/file')
 const videoSchema = require('../models/video')
 
 
-const serviceNames = require('../../serviceNames')
+const serviceNames = require('../serviceNames')
 
 const upload = require('../kapwing-api/upload')
 
