31e58d5d875c55aa56da1484ff0facd2767597c5
minhpg <67231681+minhpg@users.noreply.github.com>
2022-06-01 01:02:25 +0700

    fix

--- bunnycdn-encoding/queues/progress.js
+++ bunnycdn-encoding/queues/progress.js
@@ -2,13 +2,13 @@ const { Queue, QueueScheduler } = require('bullmq')
 const serviceNames = require('../serviceNames')
 const queue = new Queue(serviceNames.PROGRESS,{
     connection: require('../queueConnection'),
-    defaultJobOptions: {
-        attempts: 3,
-        backoff: {
-            type: 'fixed',
-            delay: 1000,
-        },
-    }
+    // defaultJobOptions: {
+    //     attempts: 3,
+    //     backoff: {
+    //         type: 'fixed',
+    //         delay: 1000,
+    //     },
+    // }
 })
 const queueScheduler = new QueueScheduler(serviceNames.PROGRESS);
 module.exports = queue
