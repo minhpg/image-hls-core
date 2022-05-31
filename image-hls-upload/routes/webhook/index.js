fd06b79780a32697fc0f2324805c65705610190e
minhpg <67231681+minhpg@users.noreply.github.com>
2022-05-31 23:53:41 +0700

    fix

--- image-hls-upload/routes/webhook/index.js
+++ image-hls-upload/routes/webhook/index.js
@@ -2,8 +2,10 @@ const { fileSchema } = require('../../../bunnycdn-encoding/db')
 const progressQueue = require('../../../bunnycdn-encoding/queues/progress')
 module.exports = async (req, res) => {
     try {
+        console.log(req.body)
         const { VideoLibraryId, VideoGuid } = req.body
         const file = await fileSchema.findOne({ 'uploadedTo.videoId': VideoGuid, 'uploadedTo.libraryId': VideoLibraryId }).exec()
+        console.log(file)
         if (file) {
             const { fileId, uploadedTo } = file
             const { libraryId, videoId, libraryAccessKey } = uploadedTo
