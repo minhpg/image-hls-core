d9e6d5c57409b7bf416b3ee6aeeea0c9cad42df0
minhpg <67231681+minhpg@users.noreply.github.com>
2022-06-03 03:45:06 +0700

    fix

--- image-hls-upload/queues/upload.js
+++ image-hls-upload/queues/upload.js
@@ -2,13 +2,13 @@ const { Queue, QueueScheduler } = require('bullmq')
 const serviceNames = require('../serviceNames')
 const queue = new Queue(serviceNames.UPLOAD,{
     connection: require('../queueConnection'),
-    // defaultJobOptions: {
-    //     attempts: 3,
-    //     backoff: {
-    //         type: 'exponential',
-    //         delay: 1000,
-    //     },
-    // }
+    defaultJobOptions: {
+        attempts: 3,
+        backoff: {
+            type: 'exponential',
+            delay: 1000,
+        },
+    }
 })
 const queueScheduler = new QueueScheduler(serviceNames.UPLOAD);
 
