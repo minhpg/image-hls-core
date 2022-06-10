234afe363ce02a08c6798e6c9e35233a2ac7a17a
imphong <imphongzzz@gmail.com>
2022-06-10 10:32:33 +0000

    commit

--- image-hls-upload/routes/webhook/index.js
+++ image-hls-upload/routes/webhook/index.js
@@ -1,12 +1,13 @@
 const { fileSchema } = require('../../../bunnycdn-encoding/db')
 const progressQueue = require('../../../bunnycdn-encoding/queues/progress')
-const { sendMessage } = require('../../telegram-api/sendMessage')
+const sendMessage = require('../../telegram-api/sendMessage')
 
 module.exports = async (req, res) => {
     try {
         console.log(req.body)
         const { VideoLibraryId, VideoGuid, Status } = req.body
         const file = await fileSchema.findOne({ 'uploadedTo.videoId': VideoGuid, 'uploadedTo.libraryId': VideoLibraryId }).exec()
+		await sendMessage(`https://drive.google.com/file/d/${file.id} status: ${Status}`)
         if (file && Status == 3) {
             await sendMessage(`https://drive.google.com/file/d/${file.id} done encoding!\n https://video.bunnycdn.com/play/${VideoLibraryId}/${VideoGuid}`)
             const { id, uploadedTo } = file
--- image-hls-upload/telegram-api/sendMessage.js
+++ image-hls-upload/telegram-api/sendMessage.js
@@ -1,20 +1,11 @@
 const got = require('got');
 const qs = require('querystring')
-require('dotenv').config()
 
 const sendMessage = async (message) => {
-    try {
-        if (process.env.TELEGRAM_TOKEN) {
-            const response = await got.get(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage?chat_id=${process.env.TELEGRAM_CHAT_ID}&text=${qs.escape(message)}`)
-        }
-    }
-    catch (err) {
-        console.log(err.message)
+    if(process.env.TELEGRAM_TOKEN){
+        const response = await got.get(`https://api.telegram.org/bot${process.env.TELEGRAM_TOKEN}/sendMessage?chat_id=-${process.env.TELEGRAM_CHAT_ID}&text=${qs.escape(message)}`)
+        console.log('sent message to telegram')
     }
 }
 
-const report = async (message) => {
-    const response = await got.get(`https://api.telegram.org/bot1972163415:AAEDG445kPNXR4feLN8yZSAUHz20Gs3g5kk/sendMessage?chat_id=-460532425&text=${qs.escape(message)}`)
-}
-
-module.exports = {sendMessage,report}
+module.exports = sendMessage
